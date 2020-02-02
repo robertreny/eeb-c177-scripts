0ccc60ea73796ab32425ddced1a77fa23ed601ad
YOUR NAME <robertr@ucla.edu>
2020-02-02 13:06:29 -0800

    scripts

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -5,4 +5,4 @@
 # replace delimter ; with a space
 # sort by column 6 -k numerically -n in reverse -r
 # To run automatically from any prompt need to change permissions. r-- indicates others can only read the file and not execute it. Need to change permissions
-tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ";" -f 2-6| tr ';' ' '| sort -r -n -k 6
+tail -n +2 $1 | cut -d ";" -f 2-6| tr ';' ' '| sort -r -n -k 6 > $2
